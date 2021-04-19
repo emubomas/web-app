a5f966282ea5267bda8facdd4be579ae589e1c3c
emubomas <33139194+emubomas@users.noreply.github.com>
2021-04-19 16:33:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -47,10 +47,10 @@ app.use(
    authRequired: false,
    auth0Logout: true,
    baseURL: APP_URL,
-    authorizationParams: {
-     response_type: "code id_token",
-     audience: "https://expenses-api",
-      scope: "openid profile email read:reports",
+   authorizationParams: {
+    response_type: "code id_token",
+    audience: "https://expenses-api",
+    scope: "openid profile email read:reports",
       },
  })
 );
